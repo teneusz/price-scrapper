6af43146a968ddf42318a3fb06f1c22a2d0c245e
Tenek <tenek@pop-os.localdomain>
2020-12-30 10:28:01 +0100

    init2

---
+++ src/components/mainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type MainPageProps = {
+
+}
+
+const MainPage = (props: MainPageProps) => {
+    return <div><h1>Hello there</h1><h3>General Kenobi</h3></div>
+}
+
+export default MainPage;
